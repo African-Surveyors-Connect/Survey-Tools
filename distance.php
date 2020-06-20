287f602fe7e374499619a8e6c2d31a995cc75e2c
surveyor <matingonk@gmail.com>
2020-06-20 17:35:22 +0200

    added new files

---
+++ distance.php
@@ -0,0 +1,124 @@
+<?php require('header.php');?>
+
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Distance</h1>
+          </div>
+
+          <div class="row">
+			  <div class="alert alert-primary" role="alert">
+				  All the adjustments contained within require you to have a measurement value of length before attempting any of the adjustments. Make sure you enter the correct values before finalizing your calculation
+			  </div>
+
+          </div>
+
+          <div class="row">
+
+            <div class="col-lg-6">
+
+              <!-- Default Card Example -->
+			  <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Standardization</h6>
+                </div>
+                <div class="card-body">
+                 
+                </div>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Temperature Adjustment</h6>
+                </div>
+                <div class="card-body">
+				  
+                </div>
+              </div>
+
+            </div>
+
+            <div class="col-lg-6">
+
+               <!-- Default Card Example -->
+			  <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Sag</h6>
+                </div>
+                <div class="card-body">
+				 
+                </div>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Tension / Pull</h6>
+                </div>
+                <div class="card-body">
+				 
+                </div>
+              </div>
+			</div>
+			
+			
+
+		  </div>
+
+		  <div class="row">
+
+            <div class="col-lg-6">
+
+              <!-- Default Card Example -->
+			  <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Mean Sea Level</h6>
+                </div>
+                <div class="card-body">
+                  
+                </div>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Combined</h6>
+                </div>
+                <div class="card-body">
+				   
+                </div>
+              </div>
+
+            </div>
+
+            <div class="col-lg-6">
+
+               <!-- Default Card Example -->
+			  <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Slope</h6>
+                </div>
+                <div class="card-body">
+				  
+                </div>
+              </div>
+			</div>
+
+		  </div>
+		  
+
+        </div>
+		<!-- /.container-fluid -->
+		
+
+      </div>
+      <!-- End of Main Content -->
+
+     
+
+<?php require('footer.php');?>
