d08ce13b0dd3ad40b1f8ccf3dcd998eebeef822c
surveyor <matingonk@gmail.com>
2020-06-22 19:51:46 +0200

    updated with selectables

--- angles.php
+++ angles.php
@@ -1,3 +1,125 @@
 <?php require('header.php');?>
 
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Angles</h1>
+          </div>
+
+          <div class="row">
+			  <div class="alert alert-primary" role="alert">
+				  All calculations that are involed in this category have something to do with angles. If you feel there is need to add an additional calculation 
+				  kindly do so in the <a href="#">Developer Suggest</a> section.
+			  </div>
+
+          </div>
+
+          <div class="row">
+
+            <div class="col-lg-6">
+
+              <!-- Default Card Example -->
+			  <div class="card shadow mb-4"><a href="standardisation.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Join</h6>
+                </div>
+                <div class="card-body">
+                 
+                </div></a>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4"><a href="temperature.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Polar</h6>
+                </div>
+                <div class="card-body">
+				  
+                </div></a>
+              </div>
+
+            </div>
+
+            <div class="col-lg-6">
+
+               <!-- Default Card Example -->
+			  <div class="card shadow mb-4"><a href="sag.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Intersection</h6>
+                </div>
+                <div class="card-body">
+				 
+                </div></a>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4"><a href="tension-pull.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Resection</h6>
+                </div>
+                <div class="card-body">
+				 
+                </div></a>
+              </div>
+			</div>
+			
+			
+
+		  </div>
+
+		  <div class="row">
+
+            <div class="col-lg-6">
+
+              <!-- Default Card Example -->
+			  <div class="card shadow mb-4"><a href="mean-sea-level.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Traversing</h6>
+                </div>
+                <div class="card-body">
+                  
+                </div></a>
+              </div>
+
+              <!-- Basic Card Example -->
+              <!--<div class="card shadow mb-4"><a href="combined-distance-reductions.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Combined</h6>
+                </div>
+                <div class="card-body">
+				   
+                </div></a>
+              </div>-->
+
+            </div>
+
+           <!-- <div class="col-lg-6"> -->
+
+               <!-- Default Card Example -->
+			 <!-- <div class="card shadow mb-4"><a href="slope.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Slope</h6>
+                </div>
+                <div class="card-body">
+				  
+                </div></a>
+              </div>
+			</div>-->
+
+		  </div>
+		  
+
+        </div>
+		<!-- /.container-fluid -->
+		
+
+      </div>
+      <!-- End of Main Content -->
+
+     
+
 <?php require('footer.php');?>
--- conversions.php
+++ conversions.php
@@ -1,3 +1,125 @@
 <?php require('header.php');?>
 
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Conversions</h1>
+          </div>
+
+          <div class="row">
+			  <div class="alert alert-primary" role="alert">
+				  Converting from one different unit to another made easy for you. Unit conversions other than the ones displayed are welcome 
+				  through the <a href="#">Developer Suggest </a> section.
+			  </div>
+
+          </div>
+
+          <div class="row">
+
+            <div class="col-lg-6">
+
+              <!-- Default Card Example -->
+			  <div class="card shadow mb-4"><a href="standardisation.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Coordinates</h6>
+                </div>
+                <div class="card-body">
+                 
+                </div></a>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4"><a href="temperature.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Angles</h6>
+                </div>
+                <div class="card-body">
+				  
+                </div></a>
+              </div>
+
+            </div>
+
+            <div class="col-lg-6">
+
+               <!-- Default Card Example -->
+			  <div class="card shadow mb-4"><a href="sag.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Length</h6>
+                </div>
+                <div class="card-body">
+				 
+                </div></a>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4"><a href="tension-pull.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Area</h6>
+                </div>
+                <div class="card-body">
+				 
+                </div></a>
+              </div>
+			</div>
+			
+			
+
+		  </div>
+
+		  <div class="row">
+
+            <div class="col-lg-6">
+
+              <!-- Default Card Example -->
+			  <div class="card shadow mb-4"><a href="mean-sea-level.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Volume</h6>
+                </div>
+                <div class="card-body">
+                  
+                </div></a>
+              </div>
+
+              <!-- Basic Card Example -->
+              <div class="card shadow mb-4"><a href="combined-distance-reductions.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Other</h6>
+                </div>
+                <div class="card-body">
+				   
+                </div></a>
+              </div>
+
+            </div>
+
+            <div class="col-lg-6">
+
+               <!-- Default Card Example -->
+			 <!-- <div class="card shadow mb-4"><a href="slope.php">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Slope</h6>
+                </div>
+                <div class="card-body">
+				  
+                </div></a>
+              </div>
+			</div>-->
+
+		  </div>
+		  
+
+        </div>
+		<!-- /.container-fluid -->
+		
+
+      </div>
+      <!-- End of Main Content -->
+
+     
+
 <?php require('footer.php');?>
