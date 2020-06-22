cb644e7ecaeb38b14e2a8845de98d925fc0386ba
surveyor <matingonk@gmail.com>
2020-06-22 19:51:26 +0200

    added developer suggest section for feedback

--- index.php
+++ index.php
@@ -127,34 +127,36 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Todays Word</h6>
                   <div class="dropdown no-arrow">
                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div class="dropdown-header">Dropdown Header:</div>
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-header">Surveyors Dictionary:</div>
+                      <a class="dropdown-item" href="#">Search Word</a>
+                      <a class="dropdown-item" href="#">Add Word</a>
                       <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
+                      <a class="dropdown-item" href="#">Report bias</a>
                     </div>
                   </div>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
-                  <div class="chart-pie pt-4 pb-2">
-                    <canvas id="myPieChart"></canvas>
-                  </div>
-                  <div class="mt-4 text-center small">
+                  Surveying term of the day comes here...
+                  <div class="mt-4 text-center" style="color:blue;">
+                    Share on<br>
                     <span class="mr-2">
-                      <i class="fas fa-circle text-primary"></i> Direct
+                      <i class="fab fa-facebook"></i>
                     </span>
                     <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Social
+                      <i class="fab fa-twitter"></i>
                     </span>
                     <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Referral
+                      <i class="fab fa-linkedin"></i>
+                    </span>
+                    <span class="mr-2">
+                      <i class="fab fa-whatsapp"></i>
                     </span>
                   </div>
                 </div>
