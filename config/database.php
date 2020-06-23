454b1e5a6d19545416bd34bbf0ad59bfcdb5f1ba
surveyor <matingonk@gmail.com>
2020-06-23 18:10:08 +0200

    updated database credentials

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "survey_tools";
+$username = "fzhzf_fzhzfxpv";
+$password = "nkm10636";
+$dbname = "fzhzfxpv_african_surveyor_connect";
 
 //create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
