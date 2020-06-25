05c12ade27cbc6dd84e0b633ebad1ffcedd70175
surveyor <matingonk@gmail.com>
2020-06-25 11:37:34 +0200

    added database details both on and offline

--- config/database.php
+++ config/database.php
@@ -1,8 +1,15 @@
 <?php
 $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "survey_tools";
+
+/* ====== foreign database connection ======
+$servername = "localhost";
 $username = "fzhzf_fzhzfxpv";
 $password = "nkm10636";
 $dbname = "fzhzfxpv_african_surveyor_connect";
+*/ 
 
 //create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
