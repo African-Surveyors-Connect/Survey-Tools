beeb99504ca30c947f78f109deb132681346d1d2
surveyor <matingonk@gmail.com>
2020-07-01 15:05:38 +0200

    database file update

--- config/database.php
+++ config/database.php
@@ -1,15 +1,16 @@
 <?php
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "survey_tools";
+*/
 
-/* ====== foreign database connection ======
 $servername = "localhost";
 $username = "fzhzf_fzhzfxpv";
 $password = "nkm10636";
 $dbname = "fzhzfxpv_african_surveyor_connect";
-*/ 
+
 
 //create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
