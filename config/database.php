14ce61e406589f1300e537de2e1b891867b25439
surveyor <matingonk@gmail.com>
2020-07-05 19:34:25 +0200

    database update

--- config/database.php
+++ config/database.php
@@ -1,16 +1,16 @@
 <?php
-
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "survey_tools";
+*/
 
-/*
 $servername = "localhost";
 $username = "fzhzf_fzhzfxpv";
 $password = "nkm10636";
 $dbname = "fzhzfxpv_african_surveyor_connect";
-*/
+
 
 //create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
